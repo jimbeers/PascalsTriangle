21e3fdd5e5cbd2a9674ce1f865f36610255e5912
Jim Beers <jim.beers@hp.com>
2017-06-18 19:08:39 -0700

    added new stuff

---
+++ src/com/jimbeers/BinomialExpanstion.java
@@ -0,0 +1,45 @@
+package com.jimbeers;
+
+public class BinomialExpanstion {
+	long[] multiplyers;
+	public BinomialExpanstion(int x, int y, int k) {
+		int numTerms = k+1;// there are one more terms then the number of powers.
+		PascalsTriangle t = new PascalsTriangle(k+1);
+		multiplyers = t.getPascalsTriangle();
+		// now calculate multiplyers, and multiply mulitplyers by these new ones
+		for (int cnt=0; cnt<numTerms; cnt++) {  // x multi will go from k to 0, y will go from 0 to k
+			long xMul = power(x, k-cnt);
+			long yMul = power(y, cnt);
+			multiplyers[cnt] = multiplyers[cnt]*xMul*yMul;
+		}
+	}
+	
+	public String toString() {
+		String retString = "";
+		for (int cnt=0; cnt<multiplyers.length; cnt++) {
+			retString += Long.toString(multiplyers[cnt]) + " ";
+		}
+		return retString;
+	}
+	
+	long power(long x, long y) {
+		if (y==0) {
+			return 1;
+		}
+		
+		long retValue = 1;
+		for (int cnt=0; cnt<y; cnt++) {
+			retValue*=x;
+		}
+		
+		return retValue;
+	}
+
+	public static void main(String[] args) {
+		BinomialExpanstion e = new BinomialExpanstion(3,4,5);
+		
+		System.out.print(e);
+
+	}
+
+}
---
+++ src/com/jimbeers/PascalsWithRecursion.java
@@ -0,0 +1,51 @@
+package com.jimbeers;
+
+import java.util.Arrays;
+
+public class PascalsWithRecursion {
+	private long[] valueRow;
+	private PascalsWithRecursion previousRow;
+	
+	public PascalsWithRecursion(int row) {
+		if (row == 1) {
+			valueRow = new long[] {1};
+		} else {
+			calculateRow(new PascalsWithRecursion(row-1));
+		}
+		
+		System.out.println(this);
+	}
+
+	private void calculateRow(PascalsWithRecursion pascalsWithRecursion) {
+		long[] previousRowValues = pascalsWithRecursion.getValueRow();
+		 // since index starts with 0, our last element will be the lenght of the previous row
+		int currentRowLast = previousRowValues.length; 
+		valueRow = new long[currentRowLast+1];
+		valueRow[0]=1;  // first value alway 1
+		valueRow[currentRowLast]=1;  // last value always 1
+		for (int x=1; x<currentRowLast;  x++) {
+			valueRow[x] = previousRowValues[x-1] + previousRowValues[x];
+		}
+	}
+
+	public long[] getValueRow() {
+		return valueRow;
+	}
+
+	public void setValueRow(long[] valueRow) {
+		this.valueRow = valueRow;
+	}
+
+	@Override
+	public String toString() {
+		return "PascalsWithRecursion [valueRow=" + Arrays.toString(valueRow) + "";
+	}
+	
+	
+	public static void main(String[] args) {
+		PascalsWithRecursion p = new PascalsWithRecursion(13);
+		long[] values = p.getValueRow();
+		
+	}
+
+}
